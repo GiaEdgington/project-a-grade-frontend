fb573b1dc6e772527717a8500e4a57500eb53557
Gia Edgington <gia.edgington@gmail.com>
2019-12-18 11:19:21 -0500

    handleChange term, location

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,12 +3,31 @@ import React from 'react';
 
 class Form extends React.Component {
 
+    state = {
+        term: "",
+        location: "",
+        restaurants : []
+    };
+
+    handleTerm = (event) => {
+        this.setState({
+            term: event.target.value
+        })
+    };
+
+    handleLocation = (event) => {
+        this.setState({
+            location: event.target.value
+        })
+    };
+
     render(){
+        console.log(this.state.location);
         return(
             <div>
                 <form>
-                    <input type="text" placeholder="Find Restaurants"></input>
-                    <input type="text" placeholder="Location"></input>
+                    <input type="text" placeholder="Find Restaurants" onChange={this.handleTerm}></input>
+                    <input type="text" placeholder="Location" onChange={this.handleLocation}></input>
                     <button></button>
                 </form>
             </div>

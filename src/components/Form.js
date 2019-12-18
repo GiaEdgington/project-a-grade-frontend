3b5052cfda802dc533c78f22ed8fb120e34b2525
Gia Edgington <gia.edgington@gmail.com>
2019-12-18 12:09:44 -0500

    fetch api working

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import DisplayRestaurant from '../containers/DisplayRestaurants';
 
 class Form extends React.Component {
 
@@ -21,15 +21,32 @@ class Form extends React.Component {
         })
     };
 
+    handleSubmit = (event) => {
+        event.preventDefault();
+        var term = this.state.term;
+        var location = this.state.location;
+
+        fetch(`http://localhost:3000/restaurants?term=${term}&location=${location}`)
+        .then(response => response.json())
+        .then(response => {
+            this.setState( { restaurants: response})
+        })
+    }
+
     render(){
-        console.log(this.state.location);
         return(
             <div>
-                <form>
-                    <input type="text" placeholder="Find Restaurants" onChange={this.handleTerm}></input>
-                    <input type="text" placeholder="Location" onChange={this.handleLocation}></input>
-                    <button></button>
-                </form>
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text" placeholder="Find Restaurants" onChange={this.handleTerm}></input>
+                        <input type="text" placeholder="Location" onChange={this.handleLocation}></input>
+                        <button>Search</button>
+                    </form>
+                </div>
+                <div>
+                    <DisplayRestaurant />
+                </div>
+                
             </div>
         )
     }

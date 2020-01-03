a2ec4e0e7eaec84fcdf007cb6fe1d9e588c9dc0b
Gia Edgington <gia.edgington@gmail.com>
2020-01-02 20:46:10 -0500

    display restaurants

--- src/containers/DisplayRestaurants.js
+++ src/containers/DisplayRestaurants.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import Restaurant from '../components/Restaurant';
 
 
 class DisplayRestaurants extends React.Component {
     
     
     render() {
+        const restaurants = this.props.restaurants.map((restaurant, index) => {
+            return <Restaurant key={index} name={restaurant.name} image={restaurant.image_url} rating={restaurant.rating} phone={restaurant.display_phone} location={restaurant.location.address1 + " " + restaurant.location.city + ", " + restaurant.location.zip_code}  />
+        })
         return(
             <div>
-
+                {restaurants};
             </div>
         )
     }

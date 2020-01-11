35369fba98dbcdf8befe9ca0f85a168e539af9d0
Gia Edgington <gia.edgington@gmail.com>
2020-01-11 15:09:43 -0500

    checkbox input values modified

--- src/components/Form.js
+++ src/components/Form.js
@@ -36,7 +36,7 @@ class Form extends React.Component {
         var location = this.state.location;
         var price = this.state.price;
 
-        fetch(`http://localhost:3000/restaurants?term=${term}&location=${location}&${price}`)
+        fetch(`http://localhost:3000/restaurants?term=${term}&location=${location}&price=${price}`)
         .then(response => response.json())
         .then(data => {
             //console.log(data);
@@ -45,7 +45,7 @@ class Form extends React.Component {
     }
 
     render(){
-        console.log(this.state.price);
+        //console.log(this.state.price);
         return(
             <div className="container">
                 <div className= "formContainer">
@@ -56,10 +56,10 @@ class Form extends React.Component {
                         <button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21.172 24l-7.387-7.387c-1.388.874-3.024 1.387-4.785 1.387-4.971 0-9-4.029-9-9s4.029-9 9-9 9 4.029 9 9c0 1.761-.514 3.398-1.387 4.785l7.387 7.387-2.828 2.828zm-12.172-8c3.859 0 7-3.14 7-7s-3.141-7-7-7-7 3.14-7 7 3.141 7 7 7z"/></svg></button>
                         <p>Filter by Price:</p>
                         <div className="filter">
-                            <label>$<input type="checkbox" value='$' onChange={this.handleCheck}></input></label>
-                            <label>$$<input type="checkbox" value='$$' onChange={this.handleCheck}></input></label>
-                            <label>$$$<input type="checkbox" value='$$$' onChange={this.handleCheck}></input></label>
-                            <label>$$$$<input type="checkbox" value='$$$$' onChange={this.handleCheck}></input></label>
+                            <label>$<input type="checkbox" value='1' onChange={this.handleCheck}></input></label>
+                            <label>$$<input type="checkbox" value='2' onChange={this.handleCheck}></input></label>
+                            <label>$$$<input type="checkbox" value='3' onChange={this.handleCheck}></input></label>
+                            <label>$$$$<input type="checkbox" value='4' onChange={this.handleCheck}></input></label>
                         </div>
                     </form>
                 </div>

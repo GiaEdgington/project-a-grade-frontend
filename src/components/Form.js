32147b69e5d0840612915773340b4dfb3c88a797
Gia Edgington <gia.edgington@gmail.com>
2020-01-13 21:44:33 -0500

    location placeholder

--- src/components/Form.js
+++ src/components/Form.js
@@ -38,7 +38,7 @@ class Form extends React.Component {
     searchCoordinates = (position) => {
         var term = this.state.term;
         var price = this.state.price;
-        console.log(position.coords.latitude + "," + position.coords.longitude);
+        //console.log(position.coords.latitude + "," + position.coords.longitude);
 
         fetch(`http://localhost:3000/restaurants?term=${term}&latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&price=${price}`)
         .then(response => response.json())
@@ -86,7 +86,7 @@ class Form extends React.Component {
                     <form onSubmit={this.handleSubmit}>
                         <input type="text" placeholder="Find Restaurants" onChange={this.handleTerm} ></input>
                         <span>|</span>
-                        <input type="text" placeholder="Location" onClick={this.handleClick} onChange={this.handleLocation} ></input>
+                        <input type="text" placeholder="Address, Neighborhood, City, State or Zip" onClick={this.handleClick} onChange={this.handleLocation} ></input>
                         <button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21.172 24l-7.387-7.387c-1.388.874-3.024 1.387-4.785 1.387-4.971 0-9-4.029-9-9s4.029-9 9-9 9 4.029 9 9c0 1.761-.514 3.398-1.387 4.785l7.387 7.387-2.828 2.828zm-12.172-8c3.859 0 7-3.14 7-7s-3.141-7-7-7-7 3.14-7 7 3.141 7 7 7z"/></svg></button>
                         <div className="currentLocation" onClick={this.handleCoordinates}>
                             <svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" aria-labelledby="title"
